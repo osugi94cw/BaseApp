be76d116eb40435e77e7e290b4472adcb0841358
osugi94cw <ysugizaki@cosawell.co.jp>
2016-09-08 16:09:47 +0900

    HTML,CSSを作成（編集） Cordovaのバージョンをデバッガーと合わせました。

--- www/controllers/library.controller.js
+++ www/controllers/library.controller.js
@@ -1,11 +1,11 @@
 function LibraryCtrl(){
-    navi.on('postpush', function(event){
-        alert("遷移成功")
-        var param1 = navi.topPage.data.param1;
-        var animation = navi.topPage.data.animation;
-        alert(animation);
-        alert(param1);
-    })
+    // navi.on('postpush', function(event){
+    //     alert("遷移成功")
+    //     var param1 = navi.topPage.data.param1;
+    //     var animation = navi.topPage.data.animation;
+    //     alert(animation);
+    //     alert(param1);
+    // })
 }
 
 angular
